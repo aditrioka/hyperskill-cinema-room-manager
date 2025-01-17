5ce638be6198bf8eed54ebdae4cc7121de7d3566
Adi Trioka <aditrioka@gmail.com>
2025-01-17 19:41:30 +0700

    finish first stage

--- Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
@@ -1,5 +1,20 @@
 package cinema
 
+const val TITLE = "Cinema:"
+const val NUM_OF_ROW = 7
+const val NUM_OF_COL = 8
+const val SEAT = "S"
+const val TOP_HEADER = "  1 2 3 4 5 6 7 8"
+
 fun main() {
-    // write your code here
+    println(TITLE)
+    println(TOP_HEADER)
+    for (row in 0..<NUM_OF_ROW) {
+        print("${row.inc()} ")
+        for (col in 0..<NUM_OF_COL) {
+            print(SEAT)
+            if (col < NUM_OF_COL.dec()) print(" ")
+        }
+        println()
+    }
 }
